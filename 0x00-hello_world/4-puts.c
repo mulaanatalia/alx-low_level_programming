a1188666ac581558004df93fd12b0d00ea56a460
Anatalia Neves Mula <mulaanatalia@gmail.com>
2022-09-12 12:58:58 -0700

    Updates task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,35 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 #include <string.h>
 
 
+
+/**
+ *
+ * *main - Main function 
+ *
+ * *
+ *
+ * *
+ *
+ * *Return: return 0 everytime
+ *
+ * */
+
 int main(void)
 
 {
-	char displayed[52];
 
 
-	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
 
-	puts(displayed);
+		char display[52];
 
-	return (0);
+
+
+			strcpy(display, "\"Programming is like building a multilingual puzzle");
+
+				puts(display);
+
+					return (0);
+
 }
